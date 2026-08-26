bd927ffb29272a812bf73b695a4656108e5bc679
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-26 15:02:23 -0400

    Simplify ARM32 consumer ownership checks

--- tests/unit/test_toolkit_arm32_consumer.py
+++ tests/unit/test_toolkit_arm32_consumer.py
@@ -1,15 +1,4 @@
-from nds_disassembly_toolkit.arm32 import (
-    ArmProgram as ToolkitArmProgram,
-    ArmRelocation as ToolkitArmRelocation,
-    BuiltArmProgram as ToolkitBuiltArmProgram,
-    Condition as ToolkitCondition,
-    DataOpcode as ToolkitDataOpcode,
-    Register as ToolkitRegister,
-    ShiftType as ToolkitShiftType,
-    encode_branch as toolkit_encode_branch,
-    encode_data_processing_immediate as toolkit_encode_data_processing_immediate,
-    encode_load_store as toolkit_encode_load_store,
-)
+import nds_disassembly_toolkit.arm32 as toolkit_arm32
 
 from bakugan_ds.gates.arm32 import (
     ARM_MODULE_SIZE,
@@ -27,18 +16,18 @@ from bakugan_ds.gates.arm32 import (
 
 
 def test_generic_arm32_types_and_encoders_are_toolkit_owned() -> None:
-    assert Condition is ToolkitCondition
-    assert Register is ToolkitRegister
-    assert ShiftType is ToolkitShiftType
-    assert DataOpcode is ToolkitDataOpcode
-    assert ArmRelocation is ToolkitArmRelocation
-    assert BuiltArmProgram is ToolkitBuiltArmProgram
-    assert encode_branch is toolkit_encode_branch
-    assert encode_data_processing_immediate is toolkit_encode_data_processing_immediate
-    assert encode_load_store is toolkit_encode_load_store
+    assert Condition is toolkit_arm32.Condition
+    assert Register is toolkit_arm32.Register
+    assert ShiftType is toolkit_arm32.ShiftType
+    assert DataOpcode is toolkit_arm32.DataOpcode
+    assert ArmRelocation is toolkit_arm32.ArmRelocation
+    assert BuiltArmProgram is toolkit_arm32.BuiltArmProgram
+    assert encode_branch is toolkit_arm32.encode_branch
+    assert encode_data_processing_immediate is toolkit_arm32.encode_data_processing_immediate
+    assert encode_load_store is toolkit_arm32.encode_load_store
 
 
 def test_bakugan_arm_program_keeps_fixed_module_policy() -> None:
-    assert ArmProgram is not ToolkitArmProgram
-    assert issubclass(ArmProgram, ToolkitArmProgram)
+    assert ArmProgram is not toolkit_arm32.ArmProgram
+    assert issubclass(ArmProgram, toolkit_arm32.ArmProgram)
     assert ARM_MODULE_SIZE == 0x8000
